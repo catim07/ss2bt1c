5465aca0513a6f4788ce1f012fd47f22ccd81841
catim07 <cuongdfle@gmail.com>
2024-12-09 21:37:35 +0700

    Add files via upload

---
+++ ss2bt1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    // int Dung de luu tru so nguyên
+    int age = 18;
+
+    // float Dùng de luu tru so thap phan
+    float height = 31.07;
+
+    // double Luu tru so thuc chinh xac cao
+    double distance = 31.072006;
+
+    // char Luu tru ky tu don
+    char grade = 'C';
+
+    // unsigned int Luu tru so nguyen khong dau
+    unsigned int population = 1000000;
+
+    // mang ký tu Luu tru chuoi ky tu
+    char name[] = "catim07";
+
+    // short Luu tru so nguyên nho
+    short year = 2025;
+
+    // long Luu tru so nguyen lon
+    long populationEarth = 310720060000000000000;
+
+    return 0;
+}
+
